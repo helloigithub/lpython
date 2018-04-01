6f5551e597d99ea2133d5e997b7095609cece4db
aaoomm <2895741727@qq.com>
2018-04-01 23:04:07 +0800

    add proxy.py

---
+++ proxy.py
@@ -0,0 +1,16 @@
+import urllib.request
+
+url = 'http://www.whatismyip.com.tw'
+
+
+proxy_support = urllib.request.ProxyHandler({'http':'118.212.137.135:31288'})
+
+opener = urllib.request.build_opener(proxy_support, urllib.request.HTTPHandler)
+opener.addheaders = [('User-Agent','Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36')]
+urllib.request.install_opener(opener)
+
+response = urllib.request.urlopen(url)
+
+html = response.read().decode('utf-8')
+
+print(html)
